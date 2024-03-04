8e2f42cb72b63678e86fc60b3d6a7f2da6878bdc
Luan053 <luanrschulzz@gmail.com>
2024-03-04 16:01:30 -0300

    feat: implementando migrations e criando entidade company

--- WebApi/Domain/Model/EmployeeAggregate/Employee.cs
+++ WebApi/Domain/Model/EmployeeAggregate/Employee.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebApi.Domain.Model
+namespace WebApi.Domain.Model.EmployeeAggregate
 {
     [Table("employee")]
     public class Employee
--- WebApi/Infrastructure/ConnectionContext.cs
+++ WebApi/Infrastructure/ConnectionContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using WebApi.Domain.Model;
+using WebApi.Domain.Model.CompanyAggregate;
+using WebApi.Domain.Model.EmployeeAggregate;
 
 namespace WebApi.Infrastructure
 {
     public class ConnectionContext : DbContext
     {
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Company { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql(
             "Server=localhost;" +
